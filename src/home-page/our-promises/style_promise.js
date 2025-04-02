6be233a418106ef1deb5b58c3266edbace7d2ad1
Lakshmi Prabha V <lakshmiprabhavl2005@gmail.com>
2025-04-02 10:15:43 +0530

    Update the new code js code

---
+++ src/home-page/our-promises/style_promise.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+export const style_promise = styled.div`
+    .element {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        margin-top: 5%;
+        margin-bottom: 5%;
+    }
+    .element_item {
+        width: 30%;
+        margin: 2%;
+        padding: 2%;
+        border-radius: 10px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+        background-color: white;
+    }
+    .item {
+        font-size: 2em;
+        color: #f1c40f;
+        margin-bottom: 10px;
+    }
+    .item_title {
+        font-size: 1.5em;
+        font-weight: bold;
+        margin-bottom: 10px;
+    }
+    .element_content {
+        font-size: 1em;
+        color: #7f8c8d;
+    }
+`;
